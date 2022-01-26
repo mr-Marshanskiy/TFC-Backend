7231233f62016ca7665a9cb152a94d05400968c1
mr-Marshanskiy@gmail.com <ureinhaven@yandex.ru>
2022-01-26 09:36:45 +0300

    fix: исправил подачу заявок на участие

--- ftc/api/constants.py
+++ ftc/api/constants.py
@@ -1,8 +1,6 @@
 from drf_yasg import openapi
 
-ACTIVE_STATUS = ['new', 'open', 'wait']
-
-
+ACTIVE_STATUS = [1, 2, 3]
 
 PLAYER = openapi.Parameter("player", in_=openapi.IN_QUERY, type=openapi.TYPE_INTEGER,
                            required=True, description="Игрок")
