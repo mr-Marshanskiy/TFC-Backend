897ca63119f8cd5531e2d1e1ce1af42a1447c383
mr-Marshanskiy@gmail.com <ureinhaven@yandex.ru>
2022-02-17 19:47:43 +0300

    feat: добавил логику опросам и участникам

--- ftc/events/models/survey.py
+++ ftc/events/models/survey.py
@@ -1,4 +1,7 @@
 from django.db import models
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from model_utils import FieldTracker
 
 from common.mixins.system import InfoMixin
 from events.models.event import Event
@@ -11,6 +14,7 @@ class Survey(InfoMixin):
     answer = models.BooleanField('Будет участвовать?', null=True, blank=True)
     comment = models.CharField('Комментарий к ответу', max_length=255,
                                null=True, blank=True)
+    tracker = FieldTracker()
 
     class Meta:
         verbose_name = 'Опрос к событию'
@@ -20,3 +24,22 @@ class Survey(InfoMixin):
 
     def __str__(self):
         return f'{self.event}({self.player})'
+
+
+@receiver(post_save, sender=Survey)
+def booking_post_save(sender, instance: Survey, created, **kwargs):
+    if created and instance.answer:
+        instance.event.participants.update_or_create(
+            player=instance.player, event=instance.event)
+    if instance.tracker.previous('answer') is None and instance.answer:
+        instance.event.participants.update_or_create(
+            player=instance.player, event=instance.event,
+            defaults={'confirmed': True})
+    elif instance.tracker.previous('answer') and instance.answer is False:
+        instance.event.participants.filter(
+            event=instance.event,
+            player=instance.player
+        ).delete()
+
+
+
