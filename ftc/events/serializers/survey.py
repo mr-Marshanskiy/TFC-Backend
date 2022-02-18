c0e8303d3a365da55fecaaae37653a035b3aa77c
mr-Marshanskiy@gmail.com <ureinhaven@yandex.ru>
2022-02-18 11:32:24 +0300

    feat: добавил фильтры в surveys

--- ftc/events/serializers/survey.py
+++ ftc/events/serializers/survey.py
@@ -1,15 +1,23 @@
 from rest_framework import serializers
 
 from events.models.survey import Survey
+from events.serializers.event import EventListSerializer
+from players.serializers.nested import PlayerNestedSerializer
 
 
 class SurveyListSerializer(serializers.ModelSerializer):
+    player = PlayerNestedSerializer()
+    event = EventListSerializer()
+
     class Meta:
         model = Survey
         fields = '__all__'
 
 
 class SurveyDetailSerializer(serializers.ModelSerializer):
+    player = PlayerNestedSerializer()
+    event = EventListSerializer()
+
     class Meta:
         model = Survey
         fields = '__all__'
