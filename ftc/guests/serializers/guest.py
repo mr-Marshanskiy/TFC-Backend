35e51e4116f81c3da6e0e2f7baca84e979080261
mr-Marshanskiy@gmail.com <ureinhaven@yandex.ru>
2022-03-12 21:12:40 +0300

    fix: добавил приложение Гости

--- ftc/guests/serializers/guest.py
+++ ftc/guests/serializers/guest.py
@@ -1,15 +1,15 @@
 from rest_framework import serializers
 
-from locations.models.location import Location
+from guests.models.guest import Guest
 
 
 class GuestDetailSerializer(serializers.ModelSerializer):
     class Meta:
-        model = Location
+        model = Guest
         fields = '__all__'
 
 
 class GuestSerializer(serializers.ModelSerializer):
     class Meta:
-        model = Location
+        model = Guest
         fields = ('id', 'name', 'phone', 'email')
