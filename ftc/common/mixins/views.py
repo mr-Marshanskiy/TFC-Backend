e488a2ddda8717797e849d519c9196e661c0e010
n.marshanskiy <n.marshanskiy@antonov.group>
2022-10-05 17:31:37 +0300

    fix: application

--- ftc/common/mixins/views.py
+++ ftc/common/mixins/views.py
@@ -11,7 +11,6 @@ class CRUViewSet(SelectSerializerMixin,
                  mixins.RetrieveModelMixin,
                  mixins.UpdateModelMixin,
                  viewsets.GenericViewSet):
-    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
 
 
 class ListCreateDeleteViewSet(SelectSerializerMixin,
@@ -19,29 +18,24 @@ class ListCreateDeleteViewSet(SelectSerializerMixin,
                               mixins.CreateModelMixin,
                               mixins.DestroyModelMixin,
                               viewsets.GenericViewSet):
-    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
 
 
 class CRUDViewSet(SelectSerializerMixin, ModelViewSet):
-    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
 
 
 class ListCreateViewSet(mixins.ListModelMixin, mixins.CreateModelMixin,
                         viewsets.GenericViewSet):
-    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
 
 
 class ListViewSet(SelectSerializerMixin,
                   mixins.ListModelMixin,
                   viewsets.GenericViewSet):
-    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
 
 
 class ListRetrieveUpdateViewSet(mixins.ListModelMixin,
                                 mixins.RetrieveModelMixin,
                                 mixins.UpdateModelMixin,
                                 viewsets.GenericViewSet):
-    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
 
 
 class CommonListAPIView(SelectSerializerMixin, ListAPIView):
